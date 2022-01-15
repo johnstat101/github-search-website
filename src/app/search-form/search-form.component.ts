eaa06baf0a5f580f5ef941da3a8cb45e1f5afc51
Rea <rmrea@kenya-airways.com>
2022-01-15 11:50:13 +0300

    add form-module

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchFormComponent implements OnInit {
 
+  public userInput!:string;
+
   constructor() { }
 
   ngOnInit(): void {
